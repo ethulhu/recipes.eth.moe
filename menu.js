5cd26995182a6693b6b914c702a65edfa715b1d8
Ethel Morgan <git@ethulhu.co.uk>
2019-01-19 11:29:09 +0000

    Names no-longer have to be paths in the menu

--- menu.js
+++ menu.js
@@ -1,14 +1,22 @@
 'use strict';
 
 class Menu {
-  constructor (items) {
+  constructor( items)  {
     this.items = items;
   }
 
   static parse( text ) {
     let lines = text.split( '\n' );
-    let items = lines.filter( line => line !== '' );
-    
+
+    let items = lines.filter( line => line !== '' )
+                     .map( line => {
+		       let parts = line.split( ': ' );
+		       return {
+		         name: parts[0],
+		         path: parts[1],
+		       }
+		     } );
+
     return new Menu( items );
   }
 }
